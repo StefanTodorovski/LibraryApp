3cbdbb40045fa6b2c74df095c1b21cbbfbeca665
Stefan Todorovski <stefan.aplch@outlook.com>
2023-10-27 14:58:53 +0200

    Initial commit

---
+++ 03-frontend/react-library/src/layouts/HomePage/HomePage.tsx
@@ -0,0 +1,20 @@
+import { Carousel } from "./components/Carousel";
+import { ExploreTopBooks } from "./components/ExploreTopBooks";
+import { Heroes } from "./components/Heroes";
+import { LibraryServices } from "./components/LibraryServices";
+
+export const HomePage = () => {
+
+return(
+
+    <>
+    <ExploreTopBooks/>
+    <Carousel/>
+    <Heroes/>
+    <LibraryServices/>
+    </>
+
+);
+
+
+}
